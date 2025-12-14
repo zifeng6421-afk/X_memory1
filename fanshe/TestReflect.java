dca4aa6bdf74354516870c3ec261dc8012e31474
zifeng6421-afk <zifeng6421@gmail.com>
2025-12-14 15:59:41 +0800

    test

--- fanshe/TestReflect.java
+++ fanshe/TestReflect.java
@@ -1,4 +1,18 @@
 package fanshe;
 
 public class TestReflect {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class<?> aClass = Class.forName("fanshe.Student");
+        Class<?> bClass = Student.class;
+        Class<?> cClass = new Student().getClass();
+
+        System.out.println(aClass);
+        System.out.println(bClass);
+        System.out.println(cClass);
+        System.out.println(aClass.getName());
+        System.out.println(bClass.getSimpleName());
+        System.out.println(cClass.getPackage());
+        System.out.println(cClass.getPackage().getName());
+
+    }
 }
